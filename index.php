1f3adc213ec65271c61b4d4260a2e23fe1a48a05
kiranbabucr7 <kiranbabu716@gmail.com>
2017-05-09 19:11:33 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+	$mysqli=new mysqli("localhost","root","","user_registration");
+	if($_SERVER["REQUSET METHOD"]=="POST"){
+		$username=$_POST["username"];
+		$password=$_POST["password"];
+		$query=msqli_query($mysqli,"SELECT * FROM users WHERE username='."$username".' AND password='".$password."'");
+		numrows=mysqli_numrows($query);
+		if(numrows>=1){
+			echo 'Login successfull!!!';
+		}
+		else{
+			echo 'login failed!!!!';
+		}
+	}
+?>
+
+<html>
+	<head>
+		<title>Index</title>
+	</head>
+		
+	<body>
+		<form>
+			<input type="text" name="username" placeholder="username" required/>
+			<input type="password" name="password" placeholder="password" required/>
+			<input type="submit" value="submit"/>
+		</form>
+	</body>
+</html>
